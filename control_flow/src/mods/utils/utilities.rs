0d58ba9b229bc33f399e8c3d5f23b0ae51dcc0f5
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-02-10 23:03:05 -0500

    rust_devbranch_start02 ≡: status-> first commit

---
+++ control_flow/src/mods/utils/utilities.rs
@@ -0,0 +1,7 @@
+// utilities.rs
+// ************************************************
+
+pub fn format_print(arg: &str, num_repeat: usize) {
+    println!("{}\n", arg.repeat(num_repeat));
+}
+// ************************************************
