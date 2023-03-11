d91c0ee8c72d18df7fb5b75160d6d67c64f1b65d
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-03-10 20:58:13 -0500

    features/rust_devbranch_start02 ≡ + added doc to greet function| zero2prod/

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -5,7 +5,32 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 type Void = ();
 //? ********************************************************
-
+/// greet is an asynchronous function that takes an HttpRequest
+/// as input and returns something that implements the Responder
+/// trait 19 . A type implements the Responder trait if it can be
+/// converted into a HttpResponse - it is implemented off the shelf
+/// for a variety of common types (e.g. strings, status codes, bytes,
+/// HttpResponse, etc.) and we can roll our own implementations if needed.
+/// # Example
+///
+/// ```rust
+/// use actix_web::{web, App, HttpServer, Responder};
+///
+/// async fn greet(name: web::Path<String>) -> impl Responder {
+///     format!("Hello, {}!", name)
+/// }
+///
+/// let app = || {
+///     App::new()
+///         .service(web::resource("/{name}").to(greet))
+/// };
+///
+/// let server = HttpServer::new(app);
+///
+/// // Start the server
+/// let addr = "127.0.0.1:8080";
+/// server.bind(addr)?.run().await?;
+/// ```
 async fn greet(req: HttpRequest) -> impl Responder {
 	let name: &str = req.match_info().get("name").unwrap_or("World");
 	format!("Hello {}!", &name)
@@ -14,6 +39,7 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> Result<Void> {
+	
 	HttpServer::new(|| {
 		App::new()
 			.route("/", web::get().to(greet))
